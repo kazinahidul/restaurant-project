b088f88ac3e39c86d631f5561d7b83b569d9b6e6
Kazi Nahidul Rashid <kazinahidul999@gmail.com>
2022-06-26 14:10:33 +0600

    setup header-body-footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './header/Header.js';
+import Body from './body/Body.js';
+import Footer from './footer/Footer.js';
 
 const MainComponent = () => {
     return (
         <div>
-
+            <Header />
+            <Body />
+            <Footer />
         </div>
     )
 }
